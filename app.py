023d086f39490908dea3750782b8638551aa24e9
1Swimmer <69285903+1Swimmer@users.noreply.github.com>
2020-11-26 16:39:56 -0600

    new version
    last updated 4:39

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Import dependencies
 
 import numpy as np
-import sqlalchemy
+import homePage
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -17,7 +17,7 @@ Base = automap_base()
 
 Base.prepare(engine, reflect=True)
 
-precipitation = Base.classes.measurement
+prcpAnalysis = Base.classes.measurement
 Station= Base.classes.station
 
 app = Flask(__name__)
@@ -39,13 +39,13 @@ def sqlalchemy():
 #Precipitation route
 
 @app.route("/api/v1.0/precipitation")
-def precipitation():
+def prcpAnalysis():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of precipitation"""
     # Query all Measures
-    results = session.query(measurement.date, measurement.prcp).all()
+    results = session.query(precipitation.date, precipitation.date).all()
 
     session.close()
 
@@ -54,7 +54,7 @@ def precipitation():
     for date, prcp in results:
         precipitation_dict = {}
         precipitation_dict["date"] = date
-        precipitation_dict["prcp"] = precipitation
+        precipitation_dict["prcp"] = prcp
         all_precipitation.append(precipitation_dict)
 
     return jsonify(all_precipitation)
@@ -83,16 +83,16 @@ def stations():
 #Tobs route
 @app.route("/api/v1.0/tobs")
 
-def tobs():
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
 #Query the dates and temperature observations of the most active station for the last year of data.
-    lastdatapointdate = session.query(tobs.date).order_by(tobs.date.desc()).first()
+    lastdatapointdate = session.query(precipitation.date).order_by(precipitation.date.desc()).first()
     lastdatapointdate[0]
 
     yearago= "2016-08-23"
-    tobs_active =session.query(tobs.date, tobs.tobs).filter(tobs.date >=yearago).all()
+    tobs_active =session.query(precipitation.date, precipitation.tobs).filter(precipitation.date >=yearago).all()
 
 # Convert list of tuples into normal list
     tobs_active = list(np.ravel(tobs_active))
